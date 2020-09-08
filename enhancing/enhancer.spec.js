08fb7ee86b148337cd2076c01eba459be25aeb73
charlene-johnson <charlene.j8234@gmail.com>
2020-09-08 16:40:07 -0400

    Finished testing of repair and made the repair function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,15 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe("runs enhancement tests", () => {
+    it("repairs", () => {
+        expect(enhancer.repair({
+            name: "Sword",
+            durability: 2,
+            enhancement: 10,
+        })).toEqual({
+            name: "Sword",
+            durability: 100,
+            enhancement: 10,
+        })
+    })
+})
